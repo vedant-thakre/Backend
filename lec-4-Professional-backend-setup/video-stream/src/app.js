0e6e0c552ea0916f3e99e8808c4e5457129db510
ved-thakre <vedantthakre7@gmail.com>
2024-05-23 22:25:43 +0530

    model intialized and packages installed

--- lec-4-Professional-backend-setup/video-stream/src/app.js
+++ lec-4-Professional-backend-setup/video-stream/src/app.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import cors from 'cors';
-import cookieParser, { CookieParser} from 'cookie-parser';
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 // middlewares
-app.use({
+app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-});
+    credentials: true,
+}));
 app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ exptended: true, limit: "16kb"}));
+app.use(express.urlencoded({ extended: true, limit: "16kb"}));
 app.use(express.static("public")) // storing the static assets in the public folder
 app.use(cookieParser());
 
