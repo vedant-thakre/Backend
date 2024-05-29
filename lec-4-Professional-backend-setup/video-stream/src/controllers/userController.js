fa6cb3e785b9eea6be001b84e2bd59b89a50e7c0
ved-thakre <vedantthakre7@gmail.com>
2024-05-29 22:13:09 +0530

    Added aggregation pipeline fetched successfully

--- lec-4-Professional-backend-setup/video-stream/src/controllers/userController.js
+++ lec-4-Professional-backend-setup/video-stream/src/controllers/userController.js
@@ -270,3 +270,73 @@ export const updateAvatar = asyncHandler(async (req, res) => {
     .status(200)
     .json(new Response(200, user, "Avatar updated successfully"));
 });
+
+export const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+
+    if(!username){
+      throw new ErrorHandler(400, "Username is missing");
+    }
+
+    const channel = await User.aggregate[
+        ({
+          $match: {
+            username: username?.toLowerCase(),
+          },
+        },
+        {
+          $lookup: {
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "channel",
+            as: "subscribers",
+          },
+        },
+        {
+          $lookup: {
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "subscriber",
+            as: "subscribedTo",
+          },
+        },
+        {
+          $addFields: {
+            subscribersCount: {
+              $size: "$subscribers",
+            },
+            channelsSubscribedToCount: {
+              $size: "$subscribedTo",
+            },
+            isSubscribed: {
+              if: {$in : [ req.user?._id, "$subscribers.subscriber"]},
+              then : true,
+              else : false
+            }
+          },
+        },
+        {
+          $project: {
+            fullName: 1,
+            username: 1,
+            email: 1,
+            avatar: 1,
+            coverImage: 1,
+            subscribersCount: 1,
+            channelsSubscribedToCount: 1,
+            isSubscribed: 1
+          }
+        })
+    ];
+
+    if(!channel?.length){
+      throw new ErrorHandler(400, "Channel does not exist");
+    }
+
+    console.log("channel -->", channel);
+
+    return res
+    .status(200)
+    .json(new Response(200, channel, "Channel fetched successfuly"))
+
+})
