2b8c87c4b84c36f9eb248a0d0bd034241bf10fb6
ved-thakre <vedantthakre7@gmail.com>
2024-06-02 23:35:48 +0530

    Updated Like controllers

--- lec-4-Professional-backend-setup/video-stream/src/controllers/likeController.js
+++ lec-4-Professional-backend-setup/video-stream/src/controllers/likeController.js
@@ -47,16 +47,122 @@ export const toggleVideoLike = asyncHandler(async (req, res) => {
 });
 
 export const toggleCommentLike = asyncHandler(async (req, res) => {
- 
+   const { commentId } = req.params;
+   const { type } = req.query;
+
+   const reaction = await Like.findOne({
+     comment: commentId,
+     likedBy: req.user._id,
+   });
+
+   if (!reaction) {
+     const like = await Like.create({
+       comment: commentId,
+       likedBy: req.user._id,
+       type: type,
+     });
+
+     return res
+       .status(200)
+       .json(
+         new Response(
+           200,
+           like,
+           `${type.charAt(0).toUpperCase() + type.slice(1)} added`
+         )
+       );
+   } else {
+     if (reaction.type !== type) {
+       existingLike.type = type;
+       const updatedLike = await existingLike.save({ new: true });
+
+       return res
+         .status(200)
+         .json(
+           new Response(
+             200,
+             updatedLike,
+             `${type.charAt(0).toUpperCase() + type.slice(1)} changed`
+           )
+         );
+     } else {
+       const removeLike = await Like.remove();
+       return res
+         .status(200)
+         .json(
+           new Response(
+             200,
+             removeLike,
+             `${type.charAt(0).toUpperCase() + type.slice(1)} removed`
+           )
+         );
+     }
+   }
 });
 
 export const toggleTweetLike = asyncHandler(async (req, res) => {
   const { tweetId } = req.params;
-  
-  // const tweet = await 
+  const { type } = req.query;
+
+  const reaction = await Like.findOne({
+    tweet: tweetId,
+    likedBy: req.user._id,
+  });
+
+  if (!reaction) {
+    const like = await Like.create({
+      tweet: tweetId,
+      likedBy: req.user._id,
+      type: type,
+    });
+
+    return res
+      .status(200)
+      .json(
+        new Response(
+          200,
+          like,
+          `${type.charAt(0).toUpperCase() + type.slice(1)} added`
+        )
+      );
+  } else {
+    if (reaction.type !== type) {
+      existingLike.type = type;
+      const updatedLike = await existingLike.save({ new: true });
+
+      return res
+        .status(200)
+        .json(
+          new Response(
+            200,
+            updatedLike,
+            `${type.charAt(0).toUpperCase() + type.slice(1)} changed`
+          )
+        );
+    } else {
+      const removeLike = await Like.remove();
+      return res
+        .status(200)
+        .json(
+          new Response(
+            200,
+            removeLike,
+            `${type.charAt(0).toUpperCase() + type.slice(1)} removed`
+          )
+        );
+    }
+  }
 });
 
 export const getLikedVideos = asyncHandler(async (req, res) => {
+  //TODO: get all liked videos
+});
+
+export const getLikedTweets = asyncHandler(async (req, res) => {
+  //TODO: get all liked videos
+});
+
+export const getLikedComments = asyncHandler(async (req, res) => {
   //TODO: get all liked videos
 });
 
