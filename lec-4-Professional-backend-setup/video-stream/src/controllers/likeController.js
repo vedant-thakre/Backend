7592ee6289100533e148edfc530fb4827bfb09d4
ved-thakre <vedantthakre7@gmail.com>
2024-06-02 23:33:31 +0530

    updated toggle video controller

--- lec-4-Professional-backend-setup/video-stream/src/controllers/likeController.js
+++ lec-4-Professional-backend-setup/video-stream/src/controllers/likeController.js
@@ -5,7 +5,45 @@ import { Response } from "../utils/responseHandler.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const toggleVideoLike = asyncHandler(async (req, res) => {
- 
+  const { videoId } = req.params;
+  const { type } = req.query;
+
+  const reaction = await Like.findOne({
+    video: videoId,
+    likedBy: req.user._id
+  });
+
+  if(!reaction){
+    const like = await Like.create({
+      video: videoId,
+      likedBy: req.user._id,
+      type: type,
+    });
+
+    return res
+    .status(200)
+    .json(
+      new Response(200, like, `${type.charAt(0).toUpperCase() + type.slice(1)} added`)
+    );
+  }else{
+    if(reaction.type !== type){
+      existingLike.type = type;
+      const updatedLike = await existingLike.save({ new: true});
+
+      return res
+        .status(200)
+        .json(
+          new Response(200, updatedLike, `${type.charAt(0).toUpperCase() + type.slice(1)} changed`)
+        );
+    }else{
+      const removeLike = await Like.remove();
+      return res
+      .status(200)
+      .json(
+        new Response(200, removeLike, `${type.charAt(0).toUpperCase() + type.slice(1)} removed`)
+      );
+    }
+  }
 });
 
 export const toggleCommentLike = asyncHandler(async (req, res) => {
