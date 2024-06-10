b556c486348a7a721af5652a44771e422ecd6ead
ved-thakre <vedantthakre7@gmail.com>
2024-06-10 23:54:27 +0530

    changes

--- lec-4-Professional-backend-setup/video-stream/src/utils/tokenGenerate.js
+++ lec-4-Professional-backend-setup/video-stream/src/utils/tokenGenerate.js
@@ -17,3 +17,19 @@ export const generateRefreshAndAccessToken = async (userId) => {
         throw new ErrorHandler(500, "Error in generating the Tokens");
     }
 }
+// export const generateRefreshAndAccessToken = async (userId) => {
+//     try {
+//         const user = await User.findById(userId);
+//         const accessToken =  user.generateAccessToken();
+//         const refreshToken = user.generateRefreshToken();
+
+//         user.refreshToken = refreshToken;
+
+//         await user.save({ validateBeforeSave: false });
+
+//         return { refreshToken, accessToken };
+//     } catch (error) {
+//         console.log("Error : ", error)
+//         throw new ErrorHandler(500, "Error in generating the Tokens");
+//     }
+// }
